3d0c1b4216d561f9090368b1baa97b053e1c25a6
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-08-21 11:08:59 +0300

    Added components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
+    HeaderComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   exports: [
     MatGridListModule,
