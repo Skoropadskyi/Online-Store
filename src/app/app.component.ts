03559759d851e3102cdf4353e9d8bb92e424771e
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-03 17:22:26 +0300

    Changed the project structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+
+import { AccountService } from './services';
+import { User } from './models';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+
+export class AppComponent {
+    user: User;
+
+    constructor(private accountService: AccountService) {
+        this.accountService.user.subscribe(x => this.user = x);
+        this.user = this.accountService.userValue;
+    }
+
+    logout() {
+        this.accountService.logout();
+    }
+}
