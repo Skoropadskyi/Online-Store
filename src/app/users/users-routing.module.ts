e7a7ff7fb1a1ba729b74b335ff3a6428fe19327a
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-04 17:06:58 +0300

    I started making a personal account.

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LayoutComponent } from './layout.component';
-import { ListComponent } from './list.component';
-import { AddEditComponent } from './add-edit.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ListComponent } from './list/list.component';
+import { AddEditComponent } from './add-edit/add-edit.component';
+import {ScoreComponent} from './score/score.component';
+import {AdsComponent} from './ads/ads.component';
 
 const routes: Routes = [
     {
@@ -11,7 +13,9 @@ const routes: Routes = [
         children: [
             { path: '', component: ListComponent },
             { path: 'add', component: AddEditComponent },
-            { path: 'edit/:id', component: AddEditComponent }
+            { path: 'edit/:id', component: AddEditComponent },
+            {path: 'score', component: ScoreComponent},
+          {path: 'ads', component: AdsComponent}
         ]
     }
 ];
