cfab5ad760a147ce583e012b48567e0d76386904
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-22 17:56:46 +0300

    added a product card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,15 +24,13 @@ import { ContentComponent } from './home/content/content.component';
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatIconModule} from "@angular/material/icon";
 import {MatMenuModule} from "@angular/material/menu";
-import { TelephoneComponent } from './home/telephone/telephone.component';;
-import { TvComponent } from './home/tv/tv.component';
-import { AudioComponent } from './home/audio/audio.component'
-;
-import { TransportComponent } from './home/transport/transport.component';
-import { LaptopsComponent } from './home/laptops/laptops.component'
-;
-import { GamingComponent } from './home/gaming/gaming.component';
-import { SmartWatchComponent } from './home/smart-watch/smart-watch.component'
+import { TelephoneComponent } from './home/telephone/telephone.component';
+import { TvComponent } from './home/tv/tv.component';
+import { AudioComponent } from './home/audio/audio.component';
+import { TransportComponent } from './home/transport/transport.component';
+import { LaptopsComponent } from './home/laptops/laptops.component';
+import { GamingComponent } from './home/gaming/gaming.component';
+import { SmartWatchComponent } from './home/smart-watch/smart-watch.component';
 @NgModule({
   imports: [
     BrowserModule,
@@ -57,14 +55,14 @@ import { SmartWatchComponent } from './home/smart-watch/smart-watch.component'
     AlertComponent,
     HomeComponent,
     ContentComponent,
-    TelephoneComponent,
-    TvComponent
-,
+    TelephoneComponent,
+    TvComponent,
     AudioComponent ,
     TransportComponent ,
-    LaptopsComponent],
-    SmartWatchComponent,
+    LaptopsComponent,
     GamingComponent,
+    SmartWatchComponent
+  ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
