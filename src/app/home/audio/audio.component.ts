ca6a3df62501560513145f56ec898b11ea06a6c3
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-25 15:33:57 +0300

    Has created blocks of products in the category Audio.

--- src/app/home/audio/audio.component.ts
+++ src/app/home/audio/audio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {products} from '@app/services/products-audio.service';
 
 @Component({
   selector: 'app-audio',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./audio.component.scss']
 })
 export class AudioComponent implements OnInit {
+
+  audio = products;
 
   constructor() { }
 
---
+++ src/app/services/products-audio.service.ts
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    name: 'Саундбар Xiaomi TV Soundbar',
+    price: 71,
+    description: '',
+    imageUrl: '../assets/img/audio/xiaomi.jpg'
+  },
+  {
+    name: 'Саундбар JBL Bar 5.1 Surround',
+    price: 465,
+    description: '',
+    imageUrl: '../assets/img/audio/jbl_5.jpg'
+  },
+  {
+    name: 'Саундбар LG SJ3',
+    price: 214,
+    description: '',
+    imageUrl: '../assets/img/audio/lg.jpg'
+  },
+  {
+    name: 'Саундбар Samsung HW-R630',
+    price: 250,
+    description: '',
+    imageUrl: '../assets/img/audio/sams_hw.jpg'
+  },
+  {
+    name: 'Саундбар JBL Bar 5.1',
+    price: 715,
+    description: '',
+    imageUrl: '../assets/img/audio/blbar5.jpg'
+  },
+  {
+    name: 'Саундбар Samsung HW-Q800T',
+    price: 715,
+    description: '',
+    imageUrl: '../assets/img/audio/sams_h.jpg'
+  },
+  {
+    name: 'Наушники Marshall Major',
+    price: 115,
+    description: '',
+    imageUrl: '../assets/img/audio/marshall.jpg'
+  },
+  {
+    name: 'Наушники Ergo BT-490',
+    price: 18,
+    description: '',
+    imageUrl: '../assets/img/audio/ergo.jpg'
+  }
+];
